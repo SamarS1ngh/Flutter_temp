d1a33087086f5636d4f2ead3fc8456d88dcbee61
Samar Singh <singhsamar2002@gmail.com>
2023-09-03 20:13:35 +0530

    Errors resolved, modified

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,19 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'ViewModel/Provider/someclass.dart';
+
 void main() {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(
-    MultiProvider(
-      providers:[
-        ChangeNotifierProvider(create(context)=> SomeChangenotifierClass())
-      ]
-    )
-    MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => SomeChangenotifierClass())
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -25,24 +26,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
-    return 
-      ScreenUtilInit(
-      designSize: Size(width,height),
-      minTextAdapt:true,
-      builder:(context,child){
-        return      
-      
-      MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-       routerConfig: AppRouter().router,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
-      }
-      );
- 
+    return ScreenUtilInit(
+        designSize: Size(width, height),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: AppRouter().router,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+          );
+        });
   }
 }
 
