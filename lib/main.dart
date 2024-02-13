674290829c49865249a15e7067c7c103015aa70e
Samar Singh <singhsamar2002@gmail.com>
2024-02-13 12:18:38 +0530

    template updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:fluttemp/Utils/Routes/app_router_config.dart';
+import 'package:fluttemp/Utils/provider/app_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'ViewModel/Provider/someclass.dart';
+import 'ViewModel/someClass_VM/someclass.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => SomeChangenotifierClass())
-    ],
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -24,38 +19,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
-    return ScreenUtilInit(
-        designSize: Size(width, height),
-        minTextAdapt: true,
-        builder: (context, child) {
-          return MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            routerConfig: AppRouter.router,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-          );
-        });
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-  });
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(),
+    return MultiProvider(
+      providers: AppProvider.providers,
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+      ),
     );
   }
 }
